8b1321be31adae9a96465ec7c3c00cabc55c261d
guntveit <mikkelguntveit@gmail.com>
2018-10-21 19:52:33 +0200

    added country list and flags + added news service

---
+++ src/news-service.js
@@ -0,0 +1,24 @@
+'use strict';
+import {
+    newsApiKey
+} from '../secrets.js';
+
+
+
+export async function getSources() {
+    const res = await fetch(`https://newsapi.org/v2/sources?apiKey=${newsApiKey}`)
+    const json = await res.json();
+    return json.sources;
+}
+
+export async function getTopHeadlinesFromSource(source) {
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${newsApiKey}`)
+    const json = await res.json();
+    return json.articles;
+}
+
+export async function getTopHeadlinesFromCountry(country) {
+    const res = await fetch(`https://newsapi.org/v2/top-headlines?country=${country}&apiKey=${newsApiKey}`)
+    const json = await res.json()
+    return json.articles;
+}
